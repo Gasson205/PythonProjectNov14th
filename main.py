dd043be844f200dc3b0cc8ff64f0c6bdcc33461e
Gasson205 <jerrygasson205@gmail.com>
2023-11-14 20:47:39 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#def greet_employee (first_name, last_name)
+def greet_employee (first_name,last_name):
+       print("Welcome! You're logged in",first_name,last_name)
+
+greet_employee(first_name="Kirara", last_name ="Carter")
+
